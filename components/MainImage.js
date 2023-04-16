1aead9afd0073498846919efbba731caf6213c95
carro <cqz@u.northwestern.edu>
2023-04-16 11:43:39 -0500

    dslfjposfjkpasf

--- components/MainImage.js
+++ components/MainImage.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import styles from '@/styles/Home.module.css';
 
-const MainImage = ({ src, alt, ...props }) => {
+const MainImage = () => {
+  const images = ['/images/bigsmile.png', '/images/open.png', '/images/smile.png', '/images/bigsmile.png']; // Store the images in an array
+  const randomIndex = Math.floor(Math.random() * images.length); // Get a random index
+  const randomSrc = images[randomIndex]; // Select a random image using the random index
   return <img style={{
     borderRadius: '10px',
     fontSize: '16px',
     cursor:'pointer',
     border: '5px solid grey',
   }} className={styles.girlImage}
-  src={src} alt={alt} {...props} />;
+  src={randomSrc} />;
 };
 
 export default MainImage;
